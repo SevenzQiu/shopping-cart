878b5cecc061a8657d56af7c8e3088e7428bbc1f
Sevenz <sevenz@SevenzdeiMac.local>
2022-04-27 14:22:02 +0800

    [新增]登入＆註冊

---
+++ backend/views/user-backend/index.php
@@ -0,0 +1,49 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use backend\models\UserBackend;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\UserBackendSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'User Backends';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-backend-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('新建用戶', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'username',
+            'auth_key',
+            'password_hash',
+            'email:email',
+            //'created_at',
+            //'updated_at',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, UserBackend $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
---
+++ common/config/main.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+
+        // 配置DB
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=shoppingCart',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 24*3600,
+            'schemaCache' => 'cache',
+        ],
+    ],
+];
